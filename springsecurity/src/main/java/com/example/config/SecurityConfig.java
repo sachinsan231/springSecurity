3f8e664b4358063070cee0a07543d0b7d60a94df
Sachin <kadam.sachin@ad.infosys.com>
2022-07-06 16:07:34 +0200

    added jdbc based user authentication

--- springsecurity/src/main/java/com/example/config/SecurityConfig.java
+++ springsecurity/src/main/java/com/example/config/SecurityConfig.java
@@ -1,11 +1,17 @@
 package com.example.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 /**
@@ -32,12 +38,25 @@ public class SecurityConfig {
 	}
 	
 	
-	@SuppressWarnings("deprecation")
-	@Bean
-	public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-		UserDetails admin = User.withDefaultPasswordEncoder().username("admin").password("12345").authorities("admin").build();
-		UserDetails user = User.withDefaultPasswordEncoder().username("user").password("12345").authorities("read").build();
-		return new InMemoryUserDetailsManager(admin, user);
-	}
+	/*
+	 * @SuppressWarnings("deprecation")
+	 * 
+	 * @Bean public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+	 * UserDetails admin =
+	 * User.withDefaultPasswordEncoder().username("admin").password("12345").
+	 * authorities("admin").build(); UserDetails user =
+	 * User.withDefaultPasswordEncoder().username("user").password("12345").
+	 * authorities("read").build(); return new InMemoryUserDetailsManager(admin,
+	 * user); }
+	 */
 
+	@Bean
+	public UserDetailsService userDetailsService(DataSource dataSource) {
+		return new JdbcUserDetailsManager(dataSource);
+	}
+	
+	@Bean
+	protected PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
 }
